0d331f134bf1b6681e2c2fbbe03c26635719674f
shoumarali <alialakbarshoumar610@gmail.com>
2025-02-25 01:08:12 +0200

    fixed mapper

--- app/src/main/java/com/alishoumar/splashcache/data/mappers/UnsplashImageMapper.kt
+++ app/src/main/java/com/alishoumar/splashcache/data/mappers/UnsplashImageMapper.kt
@@ -1,19 +1,23 @@
 package com.alishoumar.splashcache.data.mappers
 
-import androidx.paging.PagingSource
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.alishoumar.splashcache.data.models.UnsplashImageDto
 import com.alishoumar.splashcache.domain.models.UnsplashImage
 import com.alishoumar.splashcache.domain.models.Urls
 import com.alishoumar.splashcache.domain.models.User
 import com.alishoumar.splashcache.domain.models.UserLinks
 
-fun List<UnsplashImageDto>.toUnsplashImage() : List<UnsplashImage>{
+fun PagingData<UnsplashImageDto>.toUnsplashImagePagingData() : PagingData<UnsplashImage>{
     return this.map {
         UnsplashImage(
             id = it.id,
             urls = Urls(it.urls.regular),
             likes = it.likes,
-            user = User(UserLinks(it.user.userLinks.html), it.user.username)
+            user = User(
+                UserLinks(it.user.userLinks.html),
+                it.user.username
+            )
         )
     }
 }
--- app/src/main/java/com/alishoumar/splashcache/data/repository/UnsplashImageRepoImpl.kt
+++ app/src/main/java/com/alishoumar/splashcache/data/repository/UnsplashImageRepoImpl.kt
@@ -4,15 +4,17 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.PagingSourceFactory
+import androidx.paging.PagingSource
+import androidx.paging.map
 import com.alishoumar.splashcache.data.local.database.UnsplashImageDatabase
+import com.alishoumar.splashcache.data.mappers.toUnsplashImagePagingData
 import com.alishoumar.splashcache.data.models.UnsplashImageDto
 import com.alishoumar.splashcache.data.paging.UnsplashRemoteMediator
 import com.alishoumar.splashcache.data.remote.UnsplashApi
 import com.alishoumar.splashcache.data.utils.Constants.ITEMS_PER_PAGE
 import com.alishoumar.splashcache.domain.models.UnsplashImage
-import com.alishoumar.splashcache.domain.repository.UnsplashImageRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class UnsplashImageRepoImpl @Inject constructor(
@@ -20,13 +22,11 @@ class UnsplashImageRepoImpl @Inject constructor(
     private val unsplashImageDatabase: UnsplashImageDatabase
 )  {
 
-
      @OptIn(ExperimentalPagingApi::class)
-     fun getAllImages(): Flow<PagingData<UnsplashImageDto>> {
+     fun getAllImages(): Flow<PagingData<UnsplashImage>> {
         val pagingSourceFactory = {
             unsplashImageDatabase.unsplashImageDao().getAllImages()
         }
-
         return Pager(
             config = PagingConfig(pageSize =  ITEMS_PER_PAGE),
             remoteMediator = UnsplashRemoteMediator(
@@ -35,5 +35,6 @@ class UnsplashImageRepoImpl @Inject constructor(
             ),
             pagingSourceFactory = pagingSourceFactory
         ).flow
+            .map {it.toUnsplashImagePagingData()}
     }
 }
